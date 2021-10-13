c24f4d4a7dd9b0c90d1010f0738bd870266cb3f4
Coienrr <972054003@qq.com>
2021-10-13 23:29:17 +0800

    1

---
+++ docs/.vuepress/.temp/internal/clientAppEnhances.js
@@ -0,0 +1,9 @@
+import clientAppEnhance0 from 'D:/桌面文件/前端/vuepress/vuepress-starter/node_modules/@vuepress/theme-default/lib/client/clientAppEnhance.js'
+import clientAppEnhance1 from 'D:/桌面文件/前端/vuepress/vuepress-starter/node_modules/@vuepress/plugin-medium-zoom/lib/client/clientAppEnhance.js'
+import clientAppEnhance2 from 'D:/桌面文件/前端/vuepress/vuepress-starter/node_modules/@vuepress/plugin-theme-data/lib/client/clientAppEnhance.js'
+
+export const clientAppEnhances = [
+  clientAppEnhance0,
+  clientAppEnhance1,
+  clientAppEnhance2,
+]
